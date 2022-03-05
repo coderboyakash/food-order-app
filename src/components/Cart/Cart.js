5fcef94bf15fccba8f3cf5a3763dd41f3c30af0f
coderboyakash <akash.chowdhury@weavers-web.com>
2022-03-05 09:15:44 +0530

    cart done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,19 +1,46 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Modal from '../UI/Modal'
 import classes from './Cart.module.css'
-
+import CartContext from '../../store/cart-context'
+import CartItem from './CartItem'
 const Cart = (props) => {
-    const cartItems = <ul className={classes['cart-items']}>{[{id: '1', name: 'Sushi', amount: 2, price: 12.99}].map((item)=><li key={item.id}>{item.name}</li>)}</ul>
+    const cartCtx = useContext(CartContext)
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
+    const hasItems = cartCtx.items.length > 0
+
+    const cartItemRemoveHandler = id => {
+        cartCtx.removeItem(id)
+    }
+    const cartItemAddHandler = item => {
+        cartCtx.addItem(item)
+    }
+
+    const cartItems = (
+        <ul className={classes['cart-items']}>
+            {cartCtx.items.map((item) => (
+                <CartItem 
+                    key={item.id} 
+                    name={item.name}
+                    summary={item.summary}
+                    price={item.price}
+                    amount={item.amount}
+                    onAdd={cartItemAddHandler.bind(null, item.id)}
+                    onRemove={cartItemRemoveHandler.bind(null, item)}
+                />
+            ))}
+        </ul>
+    );
     return (
         <Modal onClose={props.onHideCart} className={classes.cart}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes['button--alt']} onClick={props.onHideCart}>Close</button>
-                <button className={classes.button}>Order</button>
+                {hasItems && <button className={classes.button}>Order</button>}
             </div>
         </Modal>
     )
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Input from '../../UI/Input'
 import classes from './MealItemForm.module.css'
 const MealItemForm = (props) => {
+    const amountInputRef = useRef();
+    const [amountIsValid, setAmountIsValid] = useState(true)
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const enteredAmount = amountInputRef.current.value
+        const enteredAmountNumber = +enteredAmount
+        console.log(enteredAmountNumber)
+        if(enteredAmountNumber.length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5){
+            setAmountIsValid(false)
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNumber)
+    }
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <Input
+                ref={amountInputRef}
                 label='Amount'
                 input={{ 
                     id:'amount',
@@ -15,6 +30,7 @@ const MealItemForm = (props) => {
                     defaultValue: '1'
                  }}
             />
+            {!amountIsValid && <p>Please enter a valid amount</p>}
             <button>+ Add</button>
         </form>
     )
